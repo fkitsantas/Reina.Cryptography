db84f8cbf6a7dd742828a20e69696ef427ee42fb
Fotios Kitsantas <fkitsantas@icloud.com>
2026-05-17 14:53:17 +0100

    Update to .NET 10 and xUnit v3, refactor test class
    Upgraded test project to .NET 10 and switched to xUnit v3. Refactored UnitTest1 for simplicity and added a basic assertion. Updated package references and improved code structure.

--- Reina.Cryptography.Tests/UnitTest1.cs
+++ Reina.Cryptography.Tests/UnitTest1.cs
@@ -1,11 +1,13 @@
-namespace Reina.Cryptography.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using Xunit;
 
-        }
+namespace Reina.Cryptography.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Example assertion for demonstration
+        Assert.True(true);
     }
 }
